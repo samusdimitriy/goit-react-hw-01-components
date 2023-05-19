0f5b2a6130d0374aed3f3571accde6eb0e9bbe8b
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-05-19 20:23:15 +0200

    Add src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App/App';
 import './index.css';
 
+console.log('hello');
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
